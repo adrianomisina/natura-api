4109e6ea7f9bfefb475a77743163c56260248fde
Adriano Misina <adriano.misina@gmail.com>
2021-07-05 11:19:48 -0300

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,26 @@
+//Home Scripts:
+
+//Test API Event
+document.querySelector('.getTryKey').addEventListener('click',function(e) {
+document.querySelector('.showJson').classList.toggle('showJsonOpen')
+})
+
+// Saiba Mais Test Alert 
+document.querySelector('.getting-start-button').addEventListener('click',function(e) {
+    alert('Congratulation!!! Welcome to Sensedia Natura API ;)')
+})
+
+
+//Get Start Test Alert
+document.querySelector('.integracao').addEventListener('click',function(e) {
+    alert('Integração Sensedia Natura API ;)')
+})
+
+document.querySelector('.customizacao').addEventListener('click',function(e) {
+    alert('Customização Sensedia Natura API ;)')
+})
+
+document.querySelector('.gerenciamento').addEventListener('click',function(e) {
+    alert('Gerenciamento Sensedia Natura API ;)')
+})
+
